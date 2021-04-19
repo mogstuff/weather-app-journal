b3a77da5f8dc26f52badbdcfc221ca5cd7ac6d2c
mogstuff <markmorgan.mcp@gmail.com>
2021-04-19 10:49:54 +0100

    feat: server
    created server and ran via node. console.log in callback to confirm

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8000;
+
+const server = app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  });
