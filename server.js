1daded1e24eae0a9b48b1ec791055a779263637d
mogstuff <markmorgan.mcp@gmail.com>
2021-04-20 17:32:49 +0100

    refactor: work in progress
    work in progress connecting app.js to UI

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
 
+let  apiKey = "{ YOUR API KEY HERE}";
+
+// get api key from .env file if we are running in DEV mode
+// .env file added to .gitignore to prevent security issues on githubm.com
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
+    apiKey = process.env.OW_API_KEY;
 }
 
-const apiKey = process.env.OW_API_KEY;
 
 
 const server = app.listen(port, () => {
@@ -17,7 +21,8 @@ const server = app.listen(port, () => {
 
   let projectData = {  };
 
-
+// body-parser is deprecated but leaving this in as it is still featured in the course video lessons
+// see https://stackoverflow.com/questions/24330014/bodyparser-is-deprecated-express-4
   app.use(bodyParser.urlencoded({extended:false}));
   app.use(bodyParser.json());
   app.use(cors());
--- website/app.js
+++ website/app.js
@@ -1,8 +1,43 @@
-let baseURL = "api.openweathermap.org/data/2.5/weather?zip=";
+// UI Elements
+const zip = document.getElementById("zip_code");
+const city = document.getElementById("city");
+const countryCode = document.getElementById("country_code");
+
+const weather = document.getElementById("weather");
+const date = document.getElementById("date");
+const location = document.getElementById("location");
+const temperature = document.getElementById("temperature");
+const feelingToday = document.getElementById("user_feeling");
+
+
+let baseURL = "api.openweathermap.org/data/2.5/weather?";
 let apiKey = "";
-const fetch = require("node-fetch");
 
 
+document.getElementById("generate").addEventListener("click", async (event) => {
+    
+    // guard clauses ? - call displayErrors
+
+    // set date
+
+    // set data to await getWeatherData
+
+    
+    // post data with postJournalEntry
+
+
+    // .then clause to update UI
+
+
+})
+
+
+const displayErrors = () => {
+
+}
+
+
+// get the API Key value
 const getSalt = async () => {
 
     const req = await fetch("/getsalt", {});    
@@ -13,11 +48,10 @@ const getSalt = async () => {
     }catch(error){
         console.error('cannot retrieve api key', error);
     }    
-
 }
 
-// get data from Open Weather API
-const getWeatherData = async (zip) => {
+// GET data from Open Weather API
+const getWeatherData = async (zipCode, cityName, countryCode) => {
 
     apiKey = getSalt();    
     
@@ -30,12 +64,39 @@ const getWeatherData = async (zip) => {
             console.debug('weatherData:');
             console.debug(weatherData);
             return weatherData;
-    }
-
-    catch(error){
+    }catch(error){
         console.error('error retreiving data from open weather api', error);
     }
 
 }
 
-getSalt();
+
+// POST journal entry
+// see https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+const postJournalEntry = async (url = '', data = {} ) => {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin', 
+        headers: {
+          'Content-Type': 'application/json'         
+        },
+     
+        body: JSON.stringify(data) 
+
+    });
+
+    try {
+            const weatherData = response.json();
+            return weatherData;
+
+    } catch (error) {
+        console.error("error posting data to Open Weather API", error);
+    }
+
+
+}
+
+
+
+
